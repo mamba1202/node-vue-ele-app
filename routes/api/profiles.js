2d9453dc54611fa4db06978af325ecb2ebe8e964
mamba <1612786059@qq.com>
2019-07-31 21:03:54 +0800

    获取所有以及单个信息

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -8,24 +8,54 @@ const Profile = require("../../models/Profile")
 //@desc 返回的请求的json数据
 //@access public
 router.get("/test", (req, res) => {
-     res.json({ msg: "profiles works" })
- })
+    res.json({ msg: "profiles works" })
+})
 
- //@route GET api/profiles/test
- //@desc 创建信息接口
- //@access Private
- router.get("/add",passport.authenticate('jwt',{session: false}),(req,res)=> {
-           const profileFields = {}
+//@route GET api/profiles/test
+//@desc 创建信息接口
+//@access Private
+router.get("/add", passport.authenticate('jwt', { session: false }), (req, res) => {
+    const profileFields = {}
 
-           if(req.body.type) profileFields.type = req.body.type
-           if(req.body.describe) profileFields.type = req.body.describe
-           if(req.body.income) profileFields.type = req.body.income
-           if(req.body.expend) profileFields.type = req.body.expend
-           if(req.body.cash) profileFields.type = req.body.cash
-           if(req.body.remark) profileFields.type = req.body.remark
+    if (req.body.type) profileFields.type = req.body.type
+    if (req.body.describe) profileFields.type = req.body.describe
+    if (req.body.income) profileFields.type = req.body.income
+    if (req.body.expend) profileFields.type = req.body.expend
+    if (req.body.cash) profileFields.type = req.body.cash
+    if (req.body.remark) profileFields.type = req.body.remark
 
-           new Profile(profileFields).save().then(Profile=>{
-               return json(profile)
-           })
- })
+    new Profile(profileFields).save().then(Profile => {
+        return json(profile)
+    })
+})
+
+//@route GET api/profiles
+//@desc获取所有信息
+//@access Private
+router.get("/", passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.find()   //查询所有
+        .then(profile => {
+            if (!profile) {
+                return res.status(404).json('没有任何内容')
+            }
+            res.json(profile)
+        })
+        .catch(ree => res.status(404).json('err'))
+
+})
+
+//@route GET api/profiles/:id
+//@desc获取单个信息
+//@access Private
+router.get(":id", passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOne({ _id: req.param._id })   //查询所有
+        .then(profile => {
+            if (!profile) {
+                return res.status(404).json('没有任何内容')
+            }
+            res.json(profile)
+        })
+        .catch(ree => res.status(404).json('err'))
+
+})
 module.exports = router
