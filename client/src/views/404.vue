25fa18971495a7bab093499775ab8ad61e3fb3a2
mamba <1612786059@qq.com>
2019-08-01 16:53:20 +0800

    前端项目配置

---
+++ client/src/views/404.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="notfont">
+        <img src="../assets/404.gif" alt="" srcset="">
+    </div>
+</template>
+<script>
+export default {
+    name: "404",
+    components:{}
+}
+</script>
+<style  scoped>
+.notfont{
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+}
+.notfont img{
+    width: 100%;
+    height: 100%;
+}
+</style>
+
---
+++ client/src/views/Register.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="register">
+        <section class="form_container">
+            <div class="manage_tip">
+                <span class="title">在线后台管理系统</span>
+                <el-form :model="registerUser" status-icon :rules="rules" ref="registerForm" label-width="80px" class="registerForm">
+                  <el-form-item label="用户名" prop="name">
+                   <el-input v-model="registerUser.name" placeholder="请输入用户名"></el-input>
+                </el-form-item>
+                 <el-form-item label="邮箱" prop="email">
+                   <el-input v-model="registerUser.name" placeholder="请输入邮箱"></el-input>
+                </el-form-item>
+                 <el-form-item label="密码" prop="password">
+                   <el-input type="password" v-model="registerUser.password" placeholder="请输入密码"></el-input>
+                </el-form-item>
+                 <el-form-item label="确认密码" prop="password2">
+                   <el-input type="password" v-model="registerUser.password2" placeholder="请确认密码"></el-input>
+                </el-form-item>
+
+                 <el-form-item label="选择身份">
+                 <el-select v-model="registerUser.identity" placeholder="请选择身份">
+                     <el-option label="管理员" value="manager"></el-option>
+                    <el-option label="员工" value="emplyee"></el-option>
+                 </el-select>
+                </el-form-item>
+
+         <el-form-item>
+          <el-button type="primary" class="submit_btn" @click="submitForm('registerForm')">注册</el-button>
+         </el-form-item>
+        </el-form>
+            </div>
+        </section>
+    </div>
+</template>
+<script>
+export default {
+    name: "register",
+    components:{},
+    data(){
+        return{
+            registerUser:{
+              name: '',
+              email: '',
+              password: '',
+              password2: '',
+              identity: ''
+            }
+        }
+    }
+}
+</script>
+<style scoped>
+.register {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background: url(../assets/bg.jpg) no-repeat center center;
+  background-size: 100% 100%;
+}
+.form_container {
+  width: 370px;
+  height: 210px;
+  position: absolute;
+  top: 10%;
+  left: 34%;
+  padding: 25px;
+  border-radius: 5px;
+  text-align: center;
+}
+.form_container .manage_tip .title {
+  font-family: "Microsoft YaHei";
+  font-weight: bold;
+  font-size: 26px;
+  color: #fff;
+}
+.registerForm {
+  margin-top: 20px;
+  background-color: #fff;
+  padding: 20px 40px 20px 20px;
+  border-radius: 5px;
+  box-shadow: 0px 5px 10px #cccc;
+}
+
+.submit_btn {
+  width: 100%;
+}
+</style>
+
