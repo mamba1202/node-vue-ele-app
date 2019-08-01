e4772b3744e97abda827c4a80b1338b5e88809c0
mamba <1612786059@qq.com>
2019-08-01 22:22:42 +0800

    路由守卫和token过期处理

--- client/src/http.js
+++ client/src/http.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { Message,Loading } from 'element-ui';
+import router from './router';
 
 let loading
 function startLoading() {
@@ -19,6 +20,11 @@ axios.interceptors.request.use(
     config => {
     //加载动画
     startLoading()
+
+    if(localStorage.eleToken){
+        //设置统一的请求header
+        config.headers.Authorization = localStorage.eleToken
+    }
     return config
 }, 
    error => {
@@ -37,6 +43,16 @@ error=>{
     //错误提醒
     endLoading()
     Message.error(error.response.data)
+
+    //获取错误状态码
+    const { status } = err.response
+    if(status == 401){
+        Message.error('token失效，请重新登陆！')
+        //清除token
+        localStorage.removeItem('token')
+        //跳转到登陆页面
+        router.push('/login')
+    }
     return Promise.reject(error)
 }
 )
