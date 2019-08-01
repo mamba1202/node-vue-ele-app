135aa9fecafbe83e858b7c7c7345f8e1c122d6b7
mamba <1612786059@qq.com>
2019-08-01 22:58:22 +0800

    解析token并存储的vuex中

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,33 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
-
+<script>
+import jwt from "jwt-decode";
+export default {
+  name: "app",
+  components: {},
+  created() {
+    if (localStorage.eleToken) {
+      const decode = jwt_decode(localStorage.eleToken);
+      //token存储的vuex中
+      this.$store.dispatch("setAuthenticated", !this.isEmpty(decode));
+      this.$store.dispatch("setUser", decode);
+    }
+  },
+  methods: {
+    isEmpty(value) {
+      return (
+        value === undefined ||
+        value === null ||
+        (typeof value === "object" && Object.keys(value).length === 0) ||
+        (typeof value === "string" && value.trim().length === 0)
+      );
+    }
+  }
+};
+</script>
 <style>
 html,
 body,
@@ -11,5 +35,4 @@ body,
   width: 100%;
   height: 100%;
 }
-
 </style>
