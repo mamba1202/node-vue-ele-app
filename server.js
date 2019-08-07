ff3f953ab11d41cea1796026fd75307f1bf00392
mamba <1612786059@qq.com>
2019-08-07 14:16:00 +0800

    路由配置

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
-const passport = require("process")
+const passport = require("passport")
 const app = express()
 
 //引入users.js
